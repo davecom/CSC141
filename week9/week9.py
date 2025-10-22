bc8ee858dac8ca7b9470b3be8ea6f62723d3310b
David Kopec <snpukwana@gmail.com>
2025-10-22 08:11:20 -0400

    week 9 optional parameters

--- week9/week9.py
+++ week9/week9.py
@@ -1,7 +1,13 @@
 def greet(name):
     print(f"Hi, {name}")
 
-people = ["Joe", "Mary", "Sal"]
-for person in people:
-    print(f"Hi, {name}")
+def greet2(name="Joe", times=1):
+    for time in range(times):
+        print(f"Hi, {name}")
+
+greet2()
+
+# people = ["Joe", "Mary", "Sal"]
+# for person in people:
+#     print(f"Hi, {name}")
     # greet(person)
